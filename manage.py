157d0d102559af8287022f6bb7d5b6fd6a8f436b
vernondcole <vernon.cole@ehealthnigeria.org>
2014-07-18 17:09:45 +0100

    quit tracking manage.py (2)

--- manage.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-print('''This file will be replaced by Salt upon installation.
-If you see this message, something is wrong...
-
-Such as: perhaps you did not install using SaltStack.
-''')
-import sys
-sys.stdin.readline()
